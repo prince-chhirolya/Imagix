bc5bba856042d6ae284cc30c43ec1be9b9b6ec00
Prince Chhirolya <prince.chhirolya04@gmail.com>
2024-03-05 03:19:37 +0530

    add sign-up, sign-in pages for authentication using clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import {cn} from "@/lib/utils";
 import React from "react";
+import { ClerkProvider } from '@clerk/nextjs'
 
 const IBMPlex = IBM_Plex_Sans(
     { subsets: ["latin"],
@@ -21,11 +22,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+      <ClerkProvider appearance={{
+          variables: { colorPrimary: '#624cf5'}
+      }}>
+      <html lang="en">
       <body className={cn("font-IBMPlex antialiased, IBMPlex.variable")}>
-      {children}
-      </body>
-    </html>
+          {children}
+        </body>
+      </html>
+      </ClerkProvider>
   );
 }
 
